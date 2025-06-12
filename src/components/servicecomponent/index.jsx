fb46c5f583771d334fc89a5dd31b15a618c79788
muktijindal-devnexus <muktijindal@devnexus.in>
2025-06-12 16:13:00 +0530

    changes in service section image

--- src/components/servicecomponent/index.jsx
+++ src/components/servicecomponent/index.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Banners } from "../global/NavBanners";
 import OurProcess from "./Process";
 import ServicesSection from "./ServicesSection";
-import BannerImage from '../../../public/images/services/servicesHeroSection.jpg'
 
 export const ServiceComponent = () => {
   return (
     <div>
       <Banners
-        image={BannerImage}
+        image='/images/services/servicehero.jpg'
         overlayImage
         heading="Our Services"
         description="At DevNexus Solutions, we are offering a full

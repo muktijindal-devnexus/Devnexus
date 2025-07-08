2273e1740b7f2a9d5b8a4c126126230768100fda
muktijindal-devnexus <muktijindal@devnexus.in>
2025-07-08 13:08:27 +0530

    update canonical tag

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -1,13 +1,25 @@
-import { BlogContent } from "./blog";
 
-export const metadata = {
-  title: "Blog - DevNexus Solutions",
-  description:
-    "DevNexus Solutions has worked with top clients to grow their brands and reach their desired audience.  Reach out to us today, talk with our expert today.",
-  alternates: {
-    canonical: "https://www.devnexussolutions.com/blog/${slug}",
-  },
-};
+import { BlogContent } from './blog';
+
+const slugify = (text) =>
+  (text ?? "untitled-post")
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+
+export async function generateMetadata({ params }) {
+  const slug = params.slug;
+
+  return {
+    title: `Blog - DevNexus Solutions`,
+    description:
+      'DevNexus Solutions has worked with top clients to grow their brands and reach their desired audience. Reach out to us today, talk with our expert today.',
+    alternates: {
+      canonical: `https://www.devnexussolutions.com/blog/${slug}`,
+    },
+  };
+}
 
 export default function BlogDetailPage() {
   return (

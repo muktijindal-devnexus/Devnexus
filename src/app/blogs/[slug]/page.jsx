f2572c7787e99ae0c496c9abefb07e3b6dfa596c
Mukti <muktijindal@devnexus.in>
2025-12-31 17:46:41 +0530

    fix issue of canocial

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -1,31 +1,22 @@
-
-import { BlogContent } from './blog';
-
-
-const slugify = (text) =>
-  (text ?? "untitled-post")
-    .toLowerCase()
-    .replace(/[^\w\s-]/g, "")
-    .replace(/\s+/g, "-")
-    .replace(/-+/g, "-");
+import { BlogContent } from "./blog";
 
 export async function generateMetadata({ params }) {
+  // ✅ params ko await karo
+  const resolvedParams = await params;
+  const slug = resolvedParams?.slug;
+
+  console.log("SLUG FROM METADATA:", slug);
 
   return {
     title: "Blog - DevNexus Solutions",
     description:
       "DevNexus Solutions has worked with top clients to grow their brands and reach their desired audience.",
     alternates: {
-      canonical: `https://devnexussolutions.com/blogs/${params}`,
+      canonical: `https://devnexussolutions.com/blogs/${slug}`,
     },
   };
 }
 
 export default function BlogDetailPage() {
-  return (
-    <>
-      <BlogContent />
-      
-    </>
-  );
+  return <BlogContent />;
 }

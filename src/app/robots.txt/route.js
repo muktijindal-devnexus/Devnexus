4b56557b7e325cefcb419c5655c9b61e9c90739c
Mukti <muktijindal@devnexus.in>
2026-01-06 14:09:36 +0530

    on ai

--- src/app/robots.txt/route.js
+++ src/app/robots.txt/route.js
@@ -5,23 +5,6 @@ Disallow: /user-login/
 Disallow: /register/
 Allow: /
 
-User-agent: GPTBot
-Allow: /
-
-User-agent: PerplexityBot
-Allow: /
-
-User-agent: Google-Extended
-Allow: /
-
-User-agent: ChatGPT-User
-Allow: /
-
-User-agent: DeepSeekBot
-Allow: /
-
-User-agent: ClaudeBot
-Allow: /
 
 Sitemap: https://devnexussolutions.com/sitemap.xml
 `;
